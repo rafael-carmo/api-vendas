4685e8a0e7ceddb900e9fb884a5d373768ca38ae
rafael <rafaelljj@gmail.com>
2023-02-18 11:50:34 -0300

    Seção 7: Rota de redefinição de senha.

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -2,10 +2,12 @@ import { celebrate, Segments } from 'celebrate';
 import { Router } from 'express';
 import Joi from 'joi';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const passwordRouter = Router();
 
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post(
   '/forgot',
@@ -15,6 +17,18 @@ passwordRouter.post(
     },
   }),
   forgotPasswordController.create,
+);
+
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
 );
 
 export default passwordRouter;
