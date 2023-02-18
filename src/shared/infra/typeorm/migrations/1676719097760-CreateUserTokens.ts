7ec91ada77a0d7f24a61d7dbb4c69065fa2ce65d
rafael <rafaelljj@gmail.com>
2023-02-18 08:39:24 -0300

    Seção 7: Migração da tabela user_tokens.

---
+++ src/shared/infra/typeorm/migrations/1676719097760-CreateUserTokens.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserTokens1676719097760 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TokenUser', //nome da chave estrangeira
+            referencedTableName: 'users', //nome da tabela referenciada
+            referencedColumnNames: ['id'], //coluna na tabela users
+            columnNames: ['user_id'], //coluna desta tabela (user_tokens)
+            onDelete: 'CASCADE', //quando um usuário for apagado, será apagado o user_token associado
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_tokens');
+  }
+}
