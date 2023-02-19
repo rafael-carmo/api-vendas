25e5ea24569e915fab9a6d162c6594c56c8655c7
rafael <rafaelljj@gmail.com>
2023-02-19 09:25:05 -0300

    Seção 8: Controller de perfil de usuário.

---
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,31 @@
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import { Request, Response } from 'express';
+
+export default class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const showProfile = new ShowProfileService();
+    const id = request.user.id;
+
+    const user = await showProfile.execute({ id });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const { name, email, password, old_password } = request.body;
+
+    const updateProfile = new UpdateProfileService();
+
+    const user = await updateProfile.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.json(user);
+  }
+}
